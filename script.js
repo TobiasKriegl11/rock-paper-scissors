66e653c7eaa47781a96ca9aa280d999e95a3fe4e
TobiasKriegl11 <241426819+TobiasKriegl11@users.noreply.github.com>
2026-06-02 09:02:53 +0200

    Write getComputerChoice function
    Create a function named getComputerChoice
    Use Math.random to create a number between 0 and < 1
    Multiply Math.random by 3
    Round number down with Math.floor, then add 1
    Store randomly generated number inside a variable called randomNumber
    Assign 1 to rock, 2 to paper, 3 to scissors

---
+++ script.js
@@ -0,0 +1,18 @@
+// Create a function named getComputerChoice
+//Use Math.random to create a number between 0 and < 1
+//multiply Math.random by 3
+//Round number down with Math.floor, then add 1
+//Store randomly generated number inside a variable called randomNumber
+//Assign 1 to rock, 2 to paper, 3 to scissors
+
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        return ("Rock");
+    } else if (randomNumber === 2) {
+        return ("Paper");
+    } else {
+        return ("Scissors");
+    }
+}
