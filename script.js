a4f34fda144ce49b6741c71cc771f25aec44b75c
TobiasKriegl11 <241426819+TobiasKriegl11@users.noreply.github.com>
2026-06-19 19:49:52 +0200

    Clean up code
    Swap quotes with template literals
    Use array index to clean up getComputerChoice
    Simplify playRound logic
    Create centralized playGame function
    Bundle lines for disabling/enabling buttons
    Delete obsolete code
    Organize code architecture

--- script.js
+++ script.js
@@ -1,109 +1,74 @@
-function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    if (randomNumber === 1) {
-        return ("rock");
-    } else if (randomNumber === 2) {
-        return ("paper");
-    } else {
-        return ("scissors");
-    }
-}
-
-
-function getHumanChoice () {
-    let userInput = prompt("Please enter your choice: ");
-    if (userInput === null) return "";
-    return userInput.toLowerCase();
-}
+const buttonRock = document.querySelector("#rock");   
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const results = document.querySelector("#results");
+const runningScore = document.querySelector("#runningScore");
+const checkWinnerDiv = document.querySelector("#checkWinner");
+const restart = document.querySelector("#restartButton");
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound (humanChoice, computerChoice) {
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3);
 
+    const gameOptions = ["rock", "paper", "scissors",]
+    return gameOptions[randomNumber];
+}
+
+function playRound (humanChoice, computerChoice) {
     let capitalizedHumanChoice = humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1);
     let capitalizedComputerChoice = computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1);
 
     if (humanChoice === computerChoice) {
-        results.textContent ="It's a tie! " + capitalizedHumanChoice + " draws " + computerChoice + ".";
+        results.textContent = `It's a tie! ${capitalizedHumanChoice} draws ${computerChoice}.`
         return "tie";
     } else if (humanChoice === "rock" && computerChoice === "scissors" 
         || humanChoice === "paper" && computerChoice === "rock" 
         || humanChoice === "scissors" && computerChoice === "paper") {
-        results.textContent = "You win! " + capitalizedHumanChoice + " beats " + computerChoice + "."; 
+        results.textContent = `You win! ${capitalizedHumanChoice} beats ${computerChoice}.`
         return "human";      
-    } else if (humanChoice === "rock" && computerChoice === "paper"
-        || humanChoice === "paper" && computerChoice === "scissors"
-        || humanChoice === "scissors" && computerChoice === "rock") {
-        results.textContent = "You lose! " + capitalizedComputerChoice + " beats " + humanChoice + ".";
-        return "computer"
     } else {
-        results.textContent = "Invalid entry. Please enter your choice again and check for typos.";
-        return "invalid";
-    }
-}
-
-const buttonRock = document.querySelector("#rock");   
-const buttonPaper = document.querySelector("#paper");
-const buttonScissors = document.querySelector("#scissors");
-
-buttonRock.addEventListener("click", () => {
-    const compChoice = getComputerChoice();
-    const roundWinner = playRound("rock", compChoice);
-
-    if (roundWinner === "human") humanScore++;
-    if (roundWinner === "computer") computerScore++;
-    if (roundWinner !== "human" && roundWinner !== "computer") humanScore++, computerScore++;
-
-    runningScore.textContent = "Human: " + humanScore + "; " + "Computer: " + computerScore + ";";
-
-    checkWinner();
-});
-buttonPaper.addEventListener("click", () => {
-    const compChoice = getComputerChoice();
-    const roundWinner = playRound("paper", compChoice);
-
-    if (roundWinner === "human") humanScore++;
-    if (roundWinner === "computer") computerScore++;
-    if (roundWinner !== "human" && roundWinner !== "computer") humanScore++, computerScore++;
-
-    runningScore.textContent = "Human: " + humanScore + "; " + "Computer: " + computerScore + ";";
-
-    checkWinner();
-});
-buttonScissors.addEventListener("click", () => {
-    const compChoice = getComputerChoice();
-    const roundWinner = playRound("scissors", compChoice);
-
-    if (roundWinner === "human") humanScore++;
-    if (roundWinner === "computer") computerScore++;
-    if (roundWinner !== "human" && roundWinner !== "computer") humanScore++, computerScore++;
-
-    runningScore.textContent = "Human: " + humanScore + "; " + "Computer: " + computerScore + ";";
-
-    checkWinner();
-});
-
-const results = document.querySelector("#results");
-
-const runningScore = document.querySelector("#runningScore");
-
-const checkWinnerDiv = document.querySelector("#checkWinner");
- 
-const checkWinner = function () {
-    if (humanScore === 5) {
-        checkWinnerDiv.textContent = "You won! Computer lost!"; buttonRock.disabled = true; buttonPaper.disabled = true; buttonScissors.disabled = true;
-    };
-    if (computerScore === 5) {
-        checkWinnerDiv.textContent = "You lost! Computer won!"; buttonRock.disabled = true; buttonPaper.disabled = true; buttonScissors.disabled = true;
+        results.textContent = `You lose! ${capitalizedComputerChoice} beats ${humanChoice}.`
+        return "computer"
     };
 };
 
-const restart = document.querySelector("#restartButton");
+function playGame(playerChoice) {
+    const compChoice = getComputerChoice();
+    const roundWinner = playRound(playerChoice, compChoice);
 
+    if (roundWinner === "human") humanScore++;
+    if (roundWinner === "computer") computerScore++;
+    if (roundWinner === "tie") {
+        humanScore++;
+        computerScore++;}
+
+    runningScore.textContent = `Human: ${humanScore}; Computer: ${computerScore};`;
+
+    checkWinner();
+}
+
+function checkWinner() {
+    if (humanScore === 5) {
+        checkWinnerDiv.textContent = "You won! Computer lost!"; setButtonsDisabled(true);
+    };
+    if (computerScore === 5) {
+        checkWinnerDiv.textContent = "You lost! Computer won!"; setButtonsDisabled(true);
+    };
+};
+
+function setButtonsDisabled(setting) {
+    buttonRock.disabled = setting;
+    buttonPaper.disabled = setting;
+    buttonScissors.disabled = setting;
+}
+
+buttonRock.addEventListener("click", () => playGame("rock"));
+buttonPaper.addEventListener("click", () => playGame("paper"));
+buttonScissors.addEventListener("click", () => playGame("scissors"));
 restart.addEventListener("click", () => {
-    buttonRock.disabled = false; buttonPaper.disabled = false; buttonScissors.disabled = false;
+    setButtonsDisabled(false);
     humanScore = 0; computerScore = 0;
     results.textContent = ""; runningScore.textContent = ""; checkWinnerDiv.textContent = "";
 });
